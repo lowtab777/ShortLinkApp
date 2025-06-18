cda8ccb4b40c8b89adf1b33e2edf07b4fa50d25a
lowtab777 <chikiker07@gmail.com>
2025-06-18 20:26:11 +0300

    created all other pages and service

---
+++ src/app/components/url-creation-form/url-creation-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-url-creation-form',
+  imports: [],
+  templateUrl: './url-creation-form.component.html',
+  styleUrl: './url-creation-form.component.scss'
+})
+export class UrlCreationFormComponent {
+
+}
---
+++ src/app/components/url-info/url-info.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-url-info',
+  imports: [],
+  templateUrl: './url-info.component.html',
+  styleUrl: './url-info.component.scss'
+})
+export class UrlInfoComponent {
+
+}
